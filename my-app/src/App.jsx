0451d033efa0881c79789ddd716fcd9ba76f8664
Harsh-Upadhyay123 <harsh.upadhyay_cs22@gla.ac.in>
2024-12-28 10:24:05 +0530

    28-12-24

---
+++ my-app/src/App.jsx
@@ -0,0 +1,11 @@
+export const App = () =>{
+  return (
+    <div>
+      <img src="react_photo.jpg" alt="react_photo" 
+       width="40%" height="40%"/>
+      <h1>Hello and welcomer to react series</h1>
+      <h1>Nice to meet again</h1>
+      <p>Summary:Here we are learning the basics of react.</p>
+    </div>
+  )
+};
